c556c2846373e49d4220b9c602da534bad7e909d
David Chouinard <david@davidchouinard.com>
2021-12-26 09:31:19 -0800

    Added sample endpoint

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const host = req.headers.host
+  console.log(host)
+  console.log(host.length)
+  console.log("localhost:3000".length)
+  for (var i = 0; i < host.length; i++) {
+    console.log(i, host.charAt(i), host.charCodeAt(i), host.codePointAt(i).toString(16));
+  }
+  console.log(host)
+  res.status(200).end()
 }
